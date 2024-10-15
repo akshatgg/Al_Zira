ed16c0f6dae5fae80383cda105f9ca96beb887c2
akshatgg <akshatg9636@gmail.com>
2024-10-16 00:52:59 +0530

    set up

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    // proxy: {
+    //   'api/auth/user' : {
+    //     target: "http://localhost:5000",
+    //     changeOrigin: true
+    //   }
+    // },
+        watch: {
+      usePolling: true
+    }
+  }
+})
