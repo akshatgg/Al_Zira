8064a9ae0702de4b3cc02ab2a09a84285c8caa21
akshatgg <akshatg9636@gmail.com>
2024-10-19 03:30:25 +0530

    structure created

--- src/Components/login.tsx
+++ src/Components/login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import backgroundImage from '../assets/background.svg';
-
+import Logo from "../assets/Logo.svg"
 
 export const Login: React.FC = () => {
   return (
@@ -13,6 +13,31 @@ export const Login: React.FC = () => {
         width: '100%',
       }}
     >
+    
+    <div>
+      <img src={Logo}/>
+    </div>
+
+    <div className='flex'>
+
+{/* Left side */}
+<div className='text-white flex-grow'>
+  <div>
+    Welcome Back .!
+  </div>
+  <div>
+    <button className='mt-2'> {/* Added margin-top for spacing */}
+      Skip the lag?
+    </button>
+  </div>
+</div>
+
+{/* Right side */}
+<div className='flex-none w-1/3 text-white'> {/* Adjust width as needed */}
+ jhhjj
+</div>
+    </div>
+
 
           
          
