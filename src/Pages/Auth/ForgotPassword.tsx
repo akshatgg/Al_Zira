b8eb0d7f5025ffc11e9ca28245a8614d44a9ec3d
akshatgg <akshatg9636@gmail.com>
2024-10-29 01:43:22 +0530

    adding tab view of forgot pass

--- src/Pages/Auth/ForgotPassword.tsx
+++ src/Pages/Auth/ForgotPassword.tsx
@@ -19,21 +19,22 @@ export const ForgetPassword: React.FC = () => {
             </div>
 
             <div className='flex'>
-                <div className="text-white flex-1 pl-20 pt-20 mt-20">
-                    <h1 className="text-7xl font-sans font-semibold mb-4">No Worries.!!</h1>
-                    <div className="flex items-center justify-center my-6">
-                        <button className="border border-white px-6 py-2 italic font-semibold hover:bg-white hover:text-black transition duration-300">
-                            Take me back.!
-                        </button>
-                        <div className="w-5/6 border-t-2 border-dashed border-gray-700"></div>
-                    </div>
-                </div>
+            <div className="text-white flex-1 pl-20 pt-20 mt-20">
+    <h1 className="text-4xl md:text-5xl lg:text-7xl font-sans font-semibold mb-4">No Worries.!!</h1>
+    <div className="flex items-center justify-center my-6">
+        <button className="border border-white md:px-3 lg:px-6 lg:py-2 italic font-semibold hover:bg-white hover:text-black transition duration-300">
+            Take me back.!
+        </button>
+        <div className="w-5/6 border-t-2 border-dashed border-gray-700"></div>
+    </div>
+</div>
 
 
-<div className='flex-0 pr-10'>
-                <div className=" text-white border border-white bg-transparent backdrop-filter backdrop-blur-md p-10 rounded-2xl shadow-lg">
-                    <h2 className="text-3xl font-sans font-semibold mb-0 mt-4">Forgot Password ?</h2>
-                    <p className="mb-2">Please enter your email</p>
+
+<div className='flex-0 lg:pr-10 md:pr-5'>
+                <div className=" text-white border border-white bg-transparent backdrop-filter backdrop-blur-md lg:p-10 md:p-4 rounded-2xl shadow-lg">
+                    <h2 className="lg:text-3xl md:text-2xl font-sans font-semibold mb-2 mt-4">Forgot Password ?</h2>
+                    <p className="mb-2 md:text-sm">Please enter your email</p>
 
                     <form className='mb-12'>
                         <div className="mb-4">
@@ -54,7 +55,7 @@ export const ForgetPassword: React.FC = () => {
                         </div>
                     </form>
 
-                    <div className="text-center text-sm mt-[80%]">
+                    <div className="text-center text-sm lg:mt-[80%] md:mt-[75%]">
                         Don't have an account ? <a href="/Signup" className="text-white">Signup</a>
                     </div>
 
