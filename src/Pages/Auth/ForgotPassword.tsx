5d1037221ce8c735042e75a8116711da0bcd9b23
akshatgg <akshatg9636@gmail.com>
2024-10-29 00:51:22 +0530

    location of all files are upddated

--- src/Pages/Auth/ForgotPassword.tsx
+++ src/Pages/Auth/ForgotPassword.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import backgroundImage from '../assets/ForgetPassword_Bg.svg';
-import Logo from "../assets/Logo.svg";
+import backgroundImage from '../../assets/ForgetPassword_Bg.svg';
+import Logo from "../../assets/Logo.svg";
 
 export const ForgetPassword: React.FC = () => {
     return (
--- src/Pages/Auth/login.tsx
+++ src/Pages/Auth/login.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import backgroundImage from '../assets/Login_Bg.svg';
-import Logo from "../assets/Logo.svg";
-import Google from "../assets/Google.svg";
-import Github from "../assets/Github.svg";
-import Facebook from "../assets/Facebook.svg";
+import backgroundImage from '../../assets/Login_Bg.svg';
+import Logo from "../../assets/Logo.svg";
+import Google from "../../assets/Google.svg";
+import Github from "../../assets/Github.svg";
+import Facebook from "../../assets/Facebook.svg";
 
  const Login: React.FC = () => {
    
