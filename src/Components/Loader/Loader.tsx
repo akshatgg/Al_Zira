1a19c2f6d2ce29274db32ae861c7561f57699233
akshatgg <akshatg9636@gmail.com>
2024-11-08 03:42:33 +0530

    adding jiggling process

--- src/Components/Loader/Loader.tsx
+++ src/Components/Loader/Loader.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Loader.css';
-import Logo from '../../assets/Logo.svg';
-
+import Logo from "../../assets/Logo.svg";
 export const Loader: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 5500); // Delay by 5.5 seconds
+    return () => clearTimeout(timer); // Cleanup timer on unmount
+  }, []);
+
   return (
     <div className="container">
       <div className="container2 flex item-center justify-center">
-      <img src={Logo} alt="logo" className="img" />
-      <div className="content">F.R.I.D.A.Y</div>
+        <img src={Logo} alt="logo" className="img" />
+        <div className={`content ${isVisible ? 'visible' : ''}`}>F.R.I.D.A.Y</div>
       </div>
       <div className="oval"></div>
     </div>
   );
 };
 
-Loader.displayName = 'Loader';
 export default Loader;
