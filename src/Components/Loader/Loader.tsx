53f10dec51bcf2199dbea7aaaafad25c57a1355a
akshatgg <akshatg9636@gmail.com>
2024-11-08 02:06:03 +0530

    only we have to setup the loader size and loader functionality is complete

--- src/Components/Loader/Loader.tsx
+++ src/Components/Loader/Loader.tsx
@@ -5,8 +5,10 @@ import Logo from '../../assets/Logo.svg';
 export const Loader: React.FC = () => {
   return (
     <div className="container">
+      <div className="container2 flex item-center justify-center">
       <img src={Logo} alt="logo" className="img" />
       <div className="content">F.R.I.D.A.Y</div>
+      </div>
       <div className="oval"></div>
     </div>
   );
