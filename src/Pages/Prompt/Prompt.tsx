22ad4c8039d3e66a70860a9e499b28949f3ed2fe
Rajendra Jat <jatrj18@gmail.com>
2024-11-08 08:26:30 +0530

    Updated Plus Icon Functionalities

--- src/Pages/Prompt/Prompt.tsx
+++ src/Pages/Prompt/Prompt.tsx
@@ -1,19 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SendIcon from '../../assets/send-icon.svg';
 import Media from '../../assets/media.svg';
-import Add from '../../assets/add.svg';
+import Plus from '../../assets/Plus.svg';
+import Ellipse from '../../assets/Ellipse.svg';
+import Mic from '../../assets/Mic.svg';
+import Avatar from '../../assets/Avatar Placeholder.svg';
+import Keyboard from '../../assets/Keyboard.svg';
 
 export const Prompt: React.FC = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleToggleExpand = () => {
+    setIsExpanded(prevState => !prevState);
+  };
   return (
     <div className="bg-black lg:h-screen xl:h-[calc(100vh-9vh)] md:h-h-screen h-screen text-center relative"> {/* Added relative positioning here */}
+
+      <div className="absolute top-1/4 left-10 w-10 h-10 mt-24 cursor-pointer" onClick={handleToggleExpand}>
+        {/* <div className={`relative w-full h-full transform transition-transform duration-300 ${isExpanded ? 'rotate-45' : ''}`}> */}
+
+        <div className="absolute inset-0 w-full h-full">
+          <img src={Ellipse} alt="Ellipse" className="absolute inset-0 w-full h-full" />
+        </div>
+        <div
+          className={`absolute inset-0 m-auto transition-transform duration-300 ${isExpanded ? 'rotate-45' : ''}`}
+        >
+          <img src={Plus} alt="Plus" className="absolute inset-0 m-auto text-sm p-1.5" />
+        </div>
+        {/* </div> */}
+        {isExpanded && (
+          <div className="absolute left-12 top-1/2 transform -translate-y-1/2">
+            {/* Semi-Circle Border */}
+            {/* <div className="w-40 h-20 border-t-2 border-collapse border-gray-500 rounded-t-full absolute top-0 left-0 transform -translate-x-1/2 -translate-y-full" /> */}
+            <div
+  className="w-20 h-40 border-r-2 border-gray-500 rounded-r-full absolute top-1/2 left-0 transform -translate-y-1/2"
+/>
+
+
+            {/* Mic Icon */}
+            <div className="absolute transform -translate-y-full -translate-x-1/2" style={{ transform: 'rotate(-90deg) translate(90px)' }}>
+              <div className="w-12 h-12 bg-black rounded-full border-2 border-gray-500 flex items-center justify-center">
+                <img src={Mic} alt="Microphone" className="w-6 h-6" />
+              </div>
+            </div>
+
+            {/* Keyboard Icon */}
+            <div className="absolute transform -translate-y-full" style={{ transform: 'rotate(-15deg) translate(80px)' }}>
+              <div className="w-12 h-12 bg-black rounded-full border-2 border-gray-500 flex items-center justify-center">
+                <img src={Keyboard} alt="Keyboard" className="w-6 h-6" />
+              </div>
+            </div>
+
+            {/* Avatar Icon */}
+            <div className="absolute transform -translate-y-full -translate-x-1/2" style={{ transform: 'rotate(90deg) translate(70px)' }}>
+              <div className="w-12 h-12 bg-black rounded-full border-2 border-gray-500 flex items-center justify-center">
+                <img src={Avatar} alt="Avatar" className="w-6 h-6" />
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+
       <div className="lg:space-y-4 md:space-y-3 space-y-2 xl:pt-[10%] lg:pt-[15%] md:pt-[20%] pt-[19%]"> {/* Inner container for vertical alignment */}
-        
+
         <h2 className="lg:text-4xl md:text-3xl text-2xl font-sans bg-gradient-to-r from-blue-400 to-pink-400 inline-block bg-clip-text text-transparent">
           Hello, Mukesh Anand G
         </h2>
         <p className="text-gray-400 lg:text-xl md:text-lg text-sm">What Can I Help With?</p>
 
         {/* Increased the width of the input container */}
+        {/* <div className="relative w-8 h-8">
+      <img src={Ellipse} alt="Ellipse" className="absolute inset-0 w-full h-full" />
+      <img src={Plus} alt="Plus" className="absolute inset-0 text-black m-auto text-sm cursor-pointer p-0.5" />
+    </div> */}
         <div className="relative w-full lg:max-w-3xl md:max-w-xl max-w-lg mx-auto px-4">
           <input
             type="text"
