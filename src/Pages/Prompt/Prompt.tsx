78be32734d226059af3ecfd27628a5f8702c6ca7
akshatgg <akshatg9636@gmail.com>
2024-11-20 18:42:56 +0530

    displace the position of Circular navbar

--- src/Pages/Prompt/Prompt.tsx
+++ src/Pages/Prompt/Prompt.tsx
@@ -1,24 +1,16 @@
 import React, { useState } from 'react';
 import SendIcon from '../../assets/send-icon.svg';
 import Media from '../../assets/media.svg';
-import Plus from '../../assets/Plus.svg';
-import Ellipse from '../../assets/Ellipse.svg';
-import Mic from '../../assets/Mic.svg';
-import Avatar from '../../assets/Avatar Placeholder.svg';
-import Keyboard from '../../assets/Keyboard.svg';
-import Semicircle from '../../assets/Semicircle.svg';
+
 import { useSelector } from 'react-redux';
 import { RootState } from '@reduxjs/toolkit/query';
-
+import {Circularnav} from '../../Components/Circularnav/Circularnav.tsx'
 export const Prompt: React.FC = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
   const [uploadProgress, setUploadProgress] = useState(0);
   const [uploading, setUploading] = useState(false);
   const { user, loading, error, rememberMe } = useSelector((state: RootState) => state.auth);
   
-  const handleToggleExpand = () => {
-    setIsExpanded(prevState => !prevState);
-  };
+
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -43,37 +35,9 @@ export const Prompt: React.FC = () => {
   return (
     <div className="bg-black lg:h-screen xl:h-[calc(100vh-9vh)] md:h-screen h-screen text-center relative">
       
-      <div className="expanded-items absolute top-1/4 left-10 w-10 h-10 mt-24 cursor-pointer" onClick={handleToggleExpand}>
-        <div className="absolute inset-0 w-full h-full">
-          <img src={Ellipse} alt="Ellipse" className="absolute inset-0 w-full h-full" />
-        </div>
-        <div className={`absolute inset-0 m-auto transition-transform duration-300 ${isExpanded ? 'rotate-45' : ''}`}>
-          <img src={Plus} alt="Plus" className="absolute inset-0 m-auto text-sm p-1.5" />
-        </div>
-        {isExpanded && (
-          <div className="expanded-items">
-            <img src={Semicircle} alt="semicircle" className="semicircle absolute -inset-4 transform scale-500" />
+    <Circularnav/>
 
-            <div className="icon-container absolute" style={{ transform: 'rotate(-90deg) translate(165px)' }}>
-              <div className="icon w-12 h-12 bg-black rounded-full border-2 border-gray-500 flex items-center justify-center rotate-90 hover:scale-110 hover:border-white hover:border-4 hover:bg-[0_0_15px_10px_rgba(255,0,255,0.5),0_0_25px_15px_rgba(0,255,255,0.3)] hover:shadow-[0_0_15px_10px_rgba(255,0,255,0.5),0_0_25px_15px_rgba(0,255,255,0.3)]">
-                <img src={Mic} alt="Microphone" className="icon-image w-6 h-6 hover:brightness-150" />
-              </div>
-            </div>
 
-            <div className="icon-container absolute" style={{ transform: 'rotate(0deg) translate(80px)' }}>
-              <div className="icon w-12 h-12 bg-black rounded-full border-2 border-gray-500 flex items-center justify-center hover:scale-110 hover:border-white hover:border-4 hover:bg-[0_0_15px_10px_rgba(255,0,255,0.5),0_0_25px_15px_rgba(0,255,255,0.3)] hover:shadow-[0_0_15px_10px_rgba(255,0,255,0.5),0_0_25px_15px_rgba(0,255,255,0.3)]">
-                <img src={Keyboard} alt="Keyboard" className="icon-image w-6 h-6 hover:brightness-150" />
-              </div>
-            </div>
-
-            <div className="icon-container absolute" style={{ transform: 'rotate(90deg) translate(165px)' }}>
-              <div className="icon w-12 h-12 bg-black rounded-full border-2 border-gray-500 flex items-center justify-center -rotate-90 hover:scale-110 hover:border-white hover:border-4 hover:bg-[0_0_15px_10px_rgba(255,0,255,0.5),0_0_25px_15px_rgba(0,255,255,0.3)] hover:shadow-[0_0_15px_10px_rgba(255,0,255,0.5),0_0_25px_15px_rgba(0,255,255,0.3)]">
-                <img src={Avatar} alt="Avatar" className="icon-image w-6 h-6 hover:brightness-150" />
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
 
       <div className="lg:space-y-4 md:space-y-3 space-y-2 xl:pt-[10%] lg:pt-[15%] md:pt-[20%] pt-[19%]">
         <h2 className="lg:text-4xl md:text-3xl text-2xl font-sans bg-gradient-to-r from-blue-400 to-pink-400 inline-block bg-clip-text text-transparent">
