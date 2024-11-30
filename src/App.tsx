c2f43b917a706edd6f9b125527e08363554994ab
akshatgg <akshatg9636@gmail.com>
2024-11-30 22:55:29 +0530

    removing scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 import Login from "./Pages/Auth/login.tsx";
 import Signup from "./Pages/Auth/Signup.tsx";
 import ForgotPassword from "./Pages/Auth/ForgotPassword.tsx";
-import { Navbar } from "./Pages/Navbar/Navbar.tsx";
+// import { Navbar } from "./Pages/Navbar/Navbar.tsx";
 import { Prompt } from "./Pages/Prompt/Prompt.tsx";
 import EmailVerification from "./Pages/Auth/EmailVerification.tsx";
 
@@ -14,36 +14,26 @@ import Visualizer from "./Components/Visualizer/Visualizer.tsx";
 import { AudioPrompt } from "./Pages/AudioPrompt/AudioPrompt.tsx";
 
 function App() {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
-}
-
-function AppContent() {
-  const location = useLocation();
-
-  // Check if the current route is Prompt or Welcome
-  const isSpecialRoute = ["/home", "/"].includes(location.pathname);
 
   return (
-    <div className={isSpecialRoute ? "scro" : ""}>
-      <Routes>
-        <Route path="/home" element={<Navbar />} />
-        <Route path="/loader" element={<Loader />} />
-      </Routes>
-      
-      <Routes>
-        <Route path="/home" element={<Prompt />} />
-        <Route path="/" element={<Welcome />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Signup" element={<Signup />} />
-        <Route path="/Audio" element={<AudioPrompt />} />
-        <Route path="/Forgot_Password" element={<ForgotPassword />} />
-        <Route path="/verification" element={<EmailVerification />} />
-      </Routes>
-    </div>
+
+      <Router>
+        <Routes>
+          {/* <Route path="/home" element={<Navbar />} /> */}
+          <Route path="/loader" element={<Loader />} />
+        </Routes>
+        
+        <Routes>
+          <Route path="/home" element={<Prompt />} />
+          <Route path="/" element={<Welcome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Audio" element={<AudioPrompt />} />
+          <Route path="/Forgot_Password" element={<ForgotPassword />} />
+          <Route path="/verification" element={<EmailVerification />} />
+        </Routes>
+      </Router>
+  
   );
 }
 
