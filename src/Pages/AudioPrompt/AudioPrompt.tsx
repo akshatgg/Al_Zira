4c5ea317a693b26ab5c9ff095e100ba022e6e029
akshatgg <akshatg9636@gmail.com>
2024-12-05 02:19:12 +0530

    unssaved changes

--- src/Pages/AudioPrompt/AudioPrompt.tsx
+++ src/Pages/AudioPrompt/AudioPrompt.tsx
@@ -1,35 +1,30 @@
-import React, { useRef, useState } from 'react';
-import Visualizer from '../../Components/Visualizer/Visualizer';
+import React, { useRef, useState } from "react";
+import Visualizer from "../../Components/Visualizer/Visualizer";
 import { Navbar } from "../Navbar/Navbar.tsx";
-import SpeechToText from '../../Components/SpeechToText/SpeechToText.tsx';
+import SpeechToText from "../../Components/SpeechToText/SpeechToText.tsx";
 import Mic from "../../assets/Mic.svg";
-import Keyboard from '../../assets/Keyboard.svg';
-import cross from '../../assets/cross.svg';
+import Keyboard from "../../assets/Keyboard.svg";
+import cross from "../../assets/cross.svg";
 
 export const AudioPrompt: React.FC = () => {
-  const handleResult = (text: string) => {
-    console.log("Recognized Text:", text);
-  };
+  const [listening, setListening] = useState(false);
 
-  const [listening, setListening] = useState(false); // To track if it's listening
-  const [restartListening, setRestartListening] = useState(false);
-
-  // Create a ref to access SpeechToText methods
   const speechRef = useRef<any>(null);
 
-  // Toggle start/stop listening based on the mic icon click
   const handleToggleListening = () => {
-    if (listening) {
-      setListening(false); // Stop listening
-    } else {
-      setListening(true); // Start listening
-    }
+    setListening((prevState) => !prevState);
   };
 
-  // Restart listening using the cross button
   const handleRestart = () => {
-    setRestartListening(true);
-    setListening(false);
+    if (speechRef.current) {
+      speechRef.current.stopRecognition();
+      speechRef.current.startRecognition();
+    }
+    setListening(true);
+  };
+
+  const handleResult = (text: string) => {
+    console.log("Recognized Text:", text);
   };
 
   return (
@@ -54,46 +49,40 @@ export const AudioPrompt: React.FC = () => {
           <SpeechToText
             ref={speechRef}
             startListening={listening}
-            stopListening={!listening}
-            restartListening={restartListening}
-            onResult={(text) => handleResult(text)}
+            onResult={handleResult}
           />
         </div>
       </div>
 
       {/* Bottom buttons container */}
-      <div className="absolute bottom-4  left-1/2 transform -translate-x-1/2 flex gap-14 items-center -translate-y-9">
-        {/* First icon - smaller */}
-        <div   className="icon w-8 h-8 bg-[#1C2E54] rounded-full flex items-center justify-center 
-  hover:shadow-[0px_4px_15px_rgba(255,255,255,0.3)] hover:scale-110 transition-all duration-300 ease-in-out" >
+      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex gap-14 items-center -translate-y-9">
+        <div
+          className="icon w-8 h-8 bg-[#1C2E54] rounded-full flex items-center justify-center 
+          hover:shadow-[0px_4px_15px_rgba(255,255,255,0.3)] hover:scale-110 transition-all duration-300 ease-in-out"
+        >
           <img src={Keyboard} alt="Keyboard" className="icon-image w-5 h-5 hover:brightness-150" />
         </div>
 
-        {/* Middle icon - Microphone with toggle functionality */}
         <div
-  className={`icon w-14 h-14 rounded-full border-2 border-gray-500 flex items-center justify-center 
-    hover:scale-110 hover:border-white hover:border-4 
-    bg-gradient-to-r from-[#9459B4] to-[#9487D7] 
-    ${listening ? 'shadow-[0_0_15px_10px_rgba(255,0,255,0.5),0_0_25px_15px_rgba(0,255,255,0.3)] scale-110 border-white border-4' : ''}
-    ${!listening ? 'hover:shadow-[0_0_15px_10px_rgba(255,0,255,0.5),0_0_25px_15px_rgba(0,255,255,0.3)]' : ''}`}
-  onClick={handleToggleListening}
->
-  <img src={Mic} alt="Microphone" className="icon-image w-6 h-6 hover:brightness-150" />
-</div>
+          className={`icon w-14 h-14 rounded-full border-2 border-gray-500 flex items-center justify-center 
+          hover:scale-110 hover:border-white hover:border-4 
+          bg-gradient-to-r from-[#9459B4] to-[#9487D7] 
+          ${listening ? "shadow-[0_0_15px_10px_rgba(255,0,255,0.5),0_0_25px_15px_rgba(0,255,255,0.3)] scale-110 border-white border-4" : ""}`}
+          onClick={handleToggleListening}
+        >
+          <img src={Mic} alt="Microphone" className="icon-image w-6 h-6 hover:brightness-150" />
+        </div>
 
-
-        {/* Third icon - smaller (Cross to restart) */}
         <div
-  className="icon w-8 h-8 bg-[#1C2E54] rounded-full flex items-center justify-center 
-  hover:shadow-[0px_4px_15px_rgba(255,255,255,0.3)] hover:scale-110 transition-all duration-300 ease-in-out" 
-  onClick={handleRestart}
->
-  <img src={cross} alt="Cross" className="icon-image w-5 h-5 hover:brightness-150" />
-</div>
-
+          className="icon w-8 h-8 bg-[#1C2E54] rounded-full flex items-center justify-center 
+          hover:shadow-[0px_4px_15px_rgba(255,255,255,0.3)] hover:scale-110 transition-all duration-300 ease-in-out"
+          onClick={handleRestart}
+        >
+          <img src={cross} alt="Cross" className="icon-image w-5 h-5 hover:brightness-150" />
+        </div>
       </div>
     </div>
   );
 };
 
-AudioPrompt.displayName = 'AudioPrompt';
+AudioPrompt.displayName = "AudioPrompt";
