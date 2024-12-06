39e38ca4a0dfbcbf9c2ae923c43eb901ddbe7905
akshatgg <akshatg9636@gmail.com>
2024-12-06 14:37:20 +0530

    adding Link

--- src/Components/Circularnav/Circularnav.tsx
+++ src/Components/Circularnav/Circularnav.tsx
@@ -6,7 +6,7 @@ import Mic from '../../assets/Mic.svg';
 import Avatar from '../../assets/Avatar Placeholder.svg';
 import Keyboard from '../../assets/Keyboard.svg';
 import Semicircle from '../../assets/Semicircle.svg';
-
+import {Link} from 'react-router-dom'
 export const Circularnav: React.FC = () => {
   const [isExpanded, setIsExpanded] = useState(false);
 
@@ -34,9 +34,11 @@ export const Circularnav: React.FC = () => {
           <img src={Semicircle} alt="semicircle" className="semicircle absolute -inset-4 transform scale-500" />
 
           <div className="mic icon-container absolute" style={{ transform: 'rotate(-90deg) translate(200px)' }}>
+            <Link to="/Audio">
             <div className="icon w-12 h-12 bg-black rounded-full border-2 border-gray-500 flex items-center justify-center rotate-90 hover:scale-110 hover:border-white hover:border-4 hover:bg-[0_0_15px_10px_rgba(255,0,255,0.5),0_0_25px_15px_rgba(0,255,255,0.3)] hover:shadow-[0_0_15px_10px_rgba(255,0,255,0.5),0_0_25px_15px_rgba(0,255,255,0.3)]">
               <img src={Mic} alt="Microphone" className="icon-image w-6 h-6 hover:brightness-150" />
             </div>
+            </Link>
           </div>
 
           <div className="keyboard icon-container absolute" style={{ transform: 'rotate(-25deg) translate(90px)' }}>
